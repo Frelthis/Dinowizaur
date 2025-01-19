ae69428e9a39076f6b99908f3ce97997139ebb34
Frelthis <mblumt@gmail.com>
2025-01-19 12:36:39 -0600

    basic player movement

--- dinowizaur/player.gd
+++ dinowizaur/player.gd
@@ -2,14 +2,17 @@ extends CharacterBody2D
 
 
 @export var SPEED = 300.0
+var target_velocity = Vector2.ZERO
 
 
 func _physics_process(delta):
 
-	var direction
+	var direction = Vector2.ZERO
 
-	var vel = Input.get_vector("move_left","move_right","move_forward","move_back")
+	var vel = Input.get_vector("move_left","move_right","move_up","move_down")
 	direction.x += vel.x
-	direction.z += vel.y
-
+	direction.y += vel.y
+	target_velocity.x = direction.x * SPEED
+	target_velocity.y = direction.y * SPEED
+	velocity = target_velocity
 	move_and_slide()
